14d4908dfbe942b867e770c0014dfbb117e5593d
Valentin <tolstoivalek@yandex.ru>
2025-05-03 13:02:05 +0300

    Image controller updated.

--- PetShop/Controllers/V1/Image/ImageController.cs
+++ PetShop/Controllers/V1/Image/ImageController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,8 +20,30 @@ public class ImageController
     }
     
     [HttpGet]
-    public async Task<IEnumerable<Photo>> Product(int productId)
+    public async Task<IEnumerable<PhotoDTO>> Product(int productId)
     {
-        return _context.ProductPhoto.Where(p=>p.IdProduct == productId).Include(p=>p.Photo).Select(p=>p.Photo);
+        var test = _context.ProductPhoto.Where(p=>p.IdProduct == productId).Include(p=>p.Photo).Select(p=>p.Photo).ToArray();
+        
+        if(test.Length == 0)
+            return Array.Empty<PhotoDTO>();
+        
+        var dtos = new List<PhotoDTO>();
+
+        foreach (var photo in test)
+        {
+            var base64 = Encoding.UTF8.GetString(photo.Data);
+            var dto = new PhotoDTO()
+            {
+                Data = base64,
+            };
+            dtos.Add(dto);
+        }
+        
+        return dtos.ToArray();
+    }
+
+    public class PhotoDTO
+    {
+        public string Data { get; set; }
     }
 }
